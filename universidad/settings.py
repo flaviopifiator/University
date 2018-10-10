238a7dbe3b7c22d5d3e006bad9a82386ac5a9db7
flaviopifiator <flaviomariano1993@gmail.com>
2018-10-10 12:45:22 -0300

    db

--- universidad/settings.py
+++ universidad/settings.py
@@ -88,10 +88,12 @@ DATABASES = {
         'NAME': 'universidad',
         'USER': 'universidad',
         'PASSWORD': 'universidad_123qwe',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -117,10 +119,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 USE_THOUSAND_SEPARATOR = True
-
-# Change 'default' database configuration with $DATABASE_URL.
-#DATABASES['default'] = dj_database_url.config()
-#DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
