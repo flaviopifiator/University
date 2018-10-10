407d1435aeaca2db358db68df75608fbd1f64b93
flaviopifiator <flaviomariano1993@gmail.com>
2018-10-10 06:20:12 -0300

    default config

--- universidad/settings.py
+++ universidad/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 USE_THOUSAND_SEPARATOR = True
 
 # Change 'default' database configuration with $DATABASE_URL.
-#DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
-#DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
