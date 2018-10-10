90ad7ed740fe17789aa6415881d35dbda3f2a829
flaviopifiator <flaviomariano1993@gmail.com>
2018-10-10 14:21:54 -0300

    rg

--- universidad/settings.py
+++ universidad/settings.py
@@ -82,19 +82,20 @@ WSGI_APPLICATION = 'universidad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'universidad',
-        'USER': 'universidad',
-        'PASSWORD': 'universidad_123qwe',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#        'NAME': 'universidad',
+#        'USER': 'universidad',
+#        'PASSWORD': 'universidad_123qwe',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600,
-    default='postgis://localhost:5432/{}'.format('universidad-catamarca')
+    default='postgis://localhost:5432/{}'.format('universidad')
 )
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
